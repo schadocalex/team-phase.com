6349ef1e978222d94a49e0cae2aa9013beaac1ca
Alexis Schad <schadoc_alex@hotmail.fr>
2014-08-25 01:25:38 +0200

    modifs mise en page Tournament

--- modules/tournament/tournament.php
+++ modules/tournament/tournament.php
@@ -158,39 +158,39 @@
 
 ?>
 <div id="tournament" >
+	<img src="include/img/tournament/phasethetournament.png" style="border-radius:5px;position:relative;left:1px;" width="898" />
+
 	<div class="bg4" >
-		<img src="include/img/tournament/phasethetournament.png" style="border-radius:5px;position:relative;left:-20px;top:-10px;" width="898" />
-		<p style="text-align:center;" ><a href="#popup_rules" class="fancybox" >Click here to view rules</a></p>
-	</div>
-	<div id="popup_rules" class="popup" >
-		<h2>RULES!</h2>
-		<p><center>
-			1) All participants are required to register at www.team-phase.com. A player's nickname on the website needs to match a player's in-game nickname.
-		</p>
-		<p>
-			2) The maximum amount of slots per team is 4. Teams can add a player to their roster whilst the cup is in progress as long as that player hasn't previously played for a different team.
-		</p>
-		<p>
-			3) A team is allowed to use one stand-in if required, the stand-in will however have to be approved by the admin team based on resemblance of skill level in regards to the player the stand-in is replacing.
-		</p>
-		<p>
-			4) Cheating is not allowed. Players are required to record demos of their matches and are not allowed to delete those demos until the end of the cup. An admin can at all times request those demos.
-		</p>
-		<p>
-			5) Weapon boosting is not allowed. A player committing intentional boosting will be banned from playing the next match besides further consequences based on the conditions of the event.
-		</p>
-		<p>
-			6) Allowed player classes : Medic - Lieutenant - Engineer and 1 Soldier with the exclusion of the following weapons: Panzer and Flamethrower
-		</p>
-		<p>
-			7) The games are played in the standard ABBA format. By default teams have to play 2 maps, but in the case of a draw by the end of those 2 maps a match deciding map will be played.
-			<br />If teams still draw by the end of the decider map an AB round will be added until one team comes out victorious.
-		</p>
-		<p>
-			8) During the group stage the winning teams will receive 1 point per win. The losing team won't receive any points.
-			<br />If by the end of the group stage several teams have the same amount of points their positions will be determined by the results of the games where the teams with the same amount of points played each other. Naturally the winning team will get the higher position.
-			<br />The first and second placed teams in each group will advance to the winners bracket playoffs.
-		</center></p>
+		<h2 onclick="$('#tournament_rules').fadeToggle('slow');" >RULES</h2>
+		<div id="tournament_rules" style="" >
+			<p><center>
+				1) All participants are required to register at www.team-phase.com. A player's nickname on the website needs to match a player's in-game nickname.
+			</p>
+			<p>
+				2) The maximum amount of slots per team is 4. Teams can add a player to their roster whilst the cup is in progress as long as that player hasn't previously played for a different team.
+			</p>
+			<p>
+				3) A team is allowed to use one stand-in if required, the stand-in will however have to be approved by the admin team based on resemblance of skill level in regards to the player the stand-in is replacing.
+			</p>
+			<p>
+				4) Cheating is not allowed. Players are required to record demos of their matches and are not allowed to delete those demos until the end of the cup. An admin can at all times request those demos.
+			</p>
+			<p>
+				5) Weapon boosting is not allowed. A player committing intentional boosting will be banned from playing the next match besides further consequences based on the conditions of the event.
+			</p>
+			<p>
+				6) Allowed player classes : Medic - Lieutenant - Engineer and 1 Soldier with the exclusion of the following weapons: Panzer and Flamethrower
+			</p>
+			<p>
+				7) The games are played in the standard ABBA format. By default teams have to play 2 maps, but in the case of a draw by the end of those 2 maps a match deciding map will be played.
+				<br />If teams still draw by the end of the decider map an AB round will be added until one team comes out victorious.
+			</p>
+			<p>
+				8) During the group stage the winning teams will receive 1 point per win. The losing team won't receive any points.
+				<br />If by the end of the group stage several teams have the same amount of points their positions will be determined by the results of the games where the teams with the same amount of points played each other. Naturally the winning team will get the higher position.
+				<br />The first and second placed teams in each group will advance to the winners bracket playoffs.
+			</center></p>
+		</div>
 	</div>
 	<div class="bg4" >
 		<h2>YOUR TEAM</h2>
@@ -265,35 +265,43 @@
 	<?php } ?>
 	</div>
 	<div class="bg4" >
-		<h2>TEAMS</h2>
-		<?php
-			foreach($teams as $t)
-			{
-				echo '<h3>'.dispFlag($t['flag_id']).' '.$t['name'].'</h3>';
+		<h2 onclick="$('#tournament_teams').fadeToggle('slow');" >TEAMS</h2>
+		<div id="tournament_teams" style="" >
+			<?php
+				foreach($teams as $t)
+				{
+					echo '<h3>'.dispFlag($t['flag_id']).' '.$t['name'].'</h3>';
 
-				if(!empty($t['irc']))
-					echo 'IRC: ' . $t['irc'] . '<br />';
-				if(!empty($t['website']))
-					echo 'Website: ' . $t['website'] . '<br />';
-				echo 'Members:<br/>
-					<ul>';
-				foreach ($team_users[$t['id']] as $u_array) {
-					$u_id = $u_array[0];
-					$u_status = $u_array[1];
-					$u = $users[$u_id];
-					echo '<li>'.getUsername($u);
-						if($u_status == TEAM_STATUS__MEMBER)
-							echo ' (member)';
-						if($u_status == TEAM_STATUS__LEADER)
-							echo ' (leader)';
-						if($u_status == TEAM_STATUS__ASKING)
-							echo ' (invited, waiting for answer)';
-					echo '</li>';
+					if(!empty($t['irc']))
+						echo 'IRC: ' . $t['irc'] . '<br />';
+					if(!empty($t['website']))
+						echo 'Website: ' . $t['website'] . '<br />';
+					echo 'Members:<br/>
+						<ul>';
+					foreach ($team_users[$t['id']] as $u_array) {
+						$u_id = $u_array[0];
+						$u_status = $u_array[1];
+						$u = $users[$u_id];
+						echo '<li>'.getUsername($u);
+							if($u_status == TEAM_STATUS__MEMBER)
+								echo ' (member)';
+							if($u_status == TEAM_STATUS__LEADER)
+								echo ' (leader)';
+							if($u_status == TEAM_STATUS__ASKING)
+								echo ' (invited, waiting for answer)';
+						echo '</li>';
+					}
+					echo '
+						</ul><br /><br />';
 				}
-				echo '
-					</ul><br /><br />';
-			}
-		?>
+			?>
+		</div>
+	</div>
+	<div class="bg4" >
+		<h2>GROUPS</h2>
+	</div>
+	<div class="bg4" >
+		<h2>WINNERS BRACKET</h2>
 	</div>
 </div>
 
